211a767dd9c1a4ac6d013ffec43de9074aecde4b
Robin Houston <robin.houston@gmail.com>
2015-09-23 11:50:50 +0100

    A simple example

---
+++ examples/switching_sankey_diagrams.js
@@ -0,0 +1,20 @@
+var FILES = ["first.json", "second.json"];
+var loaded_data = {},
+    number_of_files_loaded = 0;
+for (var i = 0; i < FILES.length; i++) {
+	var url = FILES[i];
+	d3.json(url, function(data) {
+		loaded_data[url] = data;
+		number_of_files_loaded += 1;
+		if (number_of_files_loaded == FILES.length) initSankey();
+	});
+}
+
+var sankey;
+function initSankey() {
+	sankey = Kiln.sankey("div").width(1200).height(500);
+	sankey.data(loaded_data["first.json"]).draw();
+}
+
+// When you want to draw the second diagram, do this:
+sankey.data(loaded_data["second.json"]).draw();
